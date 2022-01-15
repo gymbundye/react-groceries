c6924e9f43e4fd7ebf0ea52a811004a8258bce7a
gymbundye <gymbundye@gmail.com>
2022-01-14 19:35:45 -0500

    finished hw

--- src/Components/Grocerylist.js
+++ src/Components/Grocerylist.js
@@ -11,11 +11,13 @@ class Grocerylist extends Component{
             <div className="listDiv">
                 <h2>This Weeks' Grocery List</h2>
             <ul>
-               {this.props.getgroceries.map((item, index)=>{
-                    return <div key={index}>
-                        <li>Item:{item.item}</li>
-                        <li>Unit:{item.units}</li>
-                        <li>Quanity{item.quanity}</li>
+                
+               {this.props.getGroceryList.map((item, index)=>{
+                    return <div className="listDiv" key={index}>
+                       <center><input type="checkbox"/></center>
+                        <li>Item: {item.item}</li>
+                        <li>Unit: {item.units}</li>
+                        <li>Quanity: {item.quanity}</li>
                     
                     
                     </div>
